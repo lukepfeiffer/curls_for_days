dd15d0ea453353f3b5412cfe7c3da94f0a2eae79
Hashrocket Workstation <dev@hashrocket.com>
2016-07-07 13:23:14 -0400

    Add authem model and session

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'authem'
 gem 'bourbon'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.5'
---
+++ db/migrate/20160707171931_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email,                null: false
+      t.string :username
+      t.boolean :blogger
+      t.string :password_digest,      null: false
+      t.string :password_reset_token, null: false, limit: 60
+
+      t.timestamps
+    end
+  end
+end
