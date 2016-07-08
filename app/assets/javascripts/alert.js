fdb8bd41eacd152c2f6b780b1c8f474b45df25a9
Hashrocket Workstation <dev@hashrocket.com>
2016-07-08 18:48:43 -0400

    Add javascript to remove params after alerts

--- app/assets/javascripts/alert.js
+++ app/assets/javascripts/alert.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
   setTimeout(function(){
+
+    var query = window.location.search.substring(1)
+
+    if(query.length) {
+      if(window.history != undefined && window.history.pushState != undefined) {
+        window.history.pushState({}, document.title, window.location.pathname);
+      }
+    }
+
     $('.alert').fadeOut('slow');
+
   }, 5000);
 });
