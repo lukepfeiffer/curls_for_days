ef0f871d9ec99192d0a541db5472fc65d1e11547
Hashrocket Workstation <dev@hashrocket.com>
2016-07-09 12:24:46 -0400

    Add date to formatter model

--- app/models/formatter.rb
+++ app/models/formatter.rb
@@ -1,11 +1,24 @@
 class Formatter
   attr_reader :string
+  attr_reader :date
+
+  def initialize(date)
+    @date = date
+  end
 
   def initialize(string)
     @string = string
   end
 
-  def self.truncate(string)
+  def self.date(date)
+    if date.year == Date.today.year
+      date.strftime("%B %e")
+    else
+      date.strftime("%B %e, %Y")
+    end
+  end
+
+  def self.string(string)
     string_length = 125
     string[/(\S+(\s+)?){,#{string_length}}/].strip
   end
