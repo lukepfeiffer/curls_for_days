c5b30ab027bf4afcc506cb57b38486e2c9bd00e2
Hashrocket Workstation <dev@hashrocket.com>
2016-07-20 00:37:08 -0400

    User edits profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,27 +15,37 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if user.update(user_params)
+      redirect_to user_path(user)
+    else
+      redirect_to :edit
+    end
+  end
+
   def user_params
     params.require(:user).permit(
       :email,
       :password,
       :username,
-      :blogger
+      :blogger,
+      :description
     )
   end
 
   def authenticate_user
-    unauthourized_redirect_route
+    unauthorized_redirect_route
     if current_user.blank?
-      redirect_to referrer_url.to_s
-    elsif params[:id].present? && params[:id] != current_user.id
-      redirect_to referrer_url.to_s
+      redirect_to @referrer_url.to_s
+    elsif params[:id].present? && params[:id].to_i != current_user.id
+      require 'pry'; binding.pry;
+      redirect_to @referrer_url.to_s
     end
   end
 
   def unauthorized_redirect_route
-      referrer_url = URI.parse(request.referrer) rescue URI.parse('/')
+      @referrer_url = URI.parse(request.referrer) rescue URI.parse('/')
 
-      referrer_url.query = Rack::Utils.parse_nested_query(referrer_url.query).merge({notice: 'unauthorized'}).to_query
+      @referrer_url.query = Rack::Utils.parse_nested_query(@referrer_url.query).merge({notice: 'unauthorized'}).to_query
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160717145021) do
+ActiveRecord::Schema.define(version: 20160720043053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20160717145021) do
     t.string   "first_name"
     t.string   "last_name"
     t.datetime "last_subscription_check"
+    t.text     "description"
   end
 
 end
